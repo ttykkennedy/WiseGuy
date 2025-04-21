53fd84f7d950a9fe244656d80854ceb844c98000
ttykkennedy <58664513+ttykkennedy@users.noreply.github.com>
2025-04-21 12:49:59 -0400

    Update WwiseAudioSystem.h

--- WwiseAudioSystem.h
+++ WwiseAudioSystem.h
@@ -8,9 +8,7 @@ class WwiseAudioSystem
 {
 public:
     static bool Init();
-
     static void Shutdown();
-
     static void Update();
 
     static bool LoadBank(const std::string& bankName);
@@ -22,6 +20,10 @@ public:
 
     static void SetRTPCValue(const std::string& rtpcName, float value, AkGameObjectID gameObjId);
     static void SetSwitch(const std::string& switchGroup, const std::string& switchState, AkGameObjectID gameObjId);
+
+    static void SetState(const std::string& group, const std::string& state);
+    static void SetGameObjectPosition(AkGameObjectID id, float x, float y, float z = 0.0f);
+    static void SetListener(AkGameObjectID id);
 
 private:
     static bool s_isInitialized;
